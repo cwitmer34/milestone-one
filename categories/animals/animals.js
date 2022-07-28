e2d6cd728b78b10f6f269d9a775fd19a14d1d0da
cwitmer34 <54679947+cwitmer34@users.noreply.github.com>
2022-07-28 19:51:53 -0400

    Update animals.js

--- categories/animals/animals.js
+++ categories/animals/animals.js
@@ -3,12 +3,11 @@ const _URL = "https://opentdb.com/api.php?amount=1&category=27&type=multiple";
 async function getQuestion() {
   let questionData = await fetch(`${_URL}`)
   let data = await questionData.json();
-  return data
+  console.log(data.results[0], "1")
 }
 
-function displayQuestion() {
-  let question = getQuestion()
-  console.log(question.result[0])
-}
-
-displayQuestion()
+// function displayQuestion() {
+//   let question = getQuestion()
+//   console.log(question.result[0])
+// }
+getQuestion()
