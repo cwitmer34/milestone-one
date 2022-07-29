fe5c7eccc01784b67d9a2691b931e33e4d7cae0e
cwitmer34 <54679947+cwitmer34@users.noreply.github.com>
2022-07-28 20:34:27 -0400

    Fixed fetch issues.

--- categories/animals/animals.js
+++ categories/animals/animals.js
@@ -1,13 +1,20 @@
 const _URL = "https://opentdb.com/api.php?amount=1&category=27&type=multiple";
 
 async function getQuestion() {
+  console.log("fetch start")
   let questionData = await fetch(`${_URL}`)
+  console.log("fetch complete")
+  console.log("json start")
   let data = await questionData.json();
-  console.log(data.results[0], "1")
+  console.log("json complete")
+  return question = data.results[0]
 }
 
-// function displayQuestion() {
-//   let question = getQuestion()
-//   console.log(question.result[0])
-// }
-getQuestion()
+async function displayQuestion() {
+  let question = await getQuestion().then();
+  console.log(question.incorrect_answers)
+}
+
+displayQuestion()
+
+// At the time of making this comment, the OpenTDB (https://opentdb.com) API was down and I was struggling to find the structure that their objects follow to create mock data.
