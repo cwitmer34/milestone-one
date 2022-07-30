b4997e7d6651177f9819ffa563dddd9ae4b40e09
cwitmer34 <54679947+cwitmer34@users.noreply.github.com>
2022-07-30 10:47:26 -0400

    Question now displays

--- categories/animals/animals.js
+++ categories/animals/animals.js
@@ -1,20 +1,26 @@
 const _URL = "https://opentdb.com/api.php?amount=1&category=27&type=multiple";
+const _questionHTML = document.getElementById("question");
 
 async function getQuestion() {
-  console.log("fetch start")
-  let questionData = await fetch(`${_URL}`)
-  console.log("fetch complete")
-  console.log("json start")
+  let questionData = await fetch(`${_URL}`);
   let data = await questionData.json();
-  console.log("json complete")
-  return question = data.results[0]
+  return (question = data.results[0]);
+}
+
+function randomElement(array) {
+  // Credits: Jacob Relkin (https://stackoverflow.com/questions/4550505/getting-a-random-value-from-a-javascript-array)
+  return (randomElement = array[Math.floor(Math.random() * array.length)]);
+}
+
+async function appendQuestion() {
+  let question = await getQuestion().then();
+  _questionHTML.textContent = question.question;
 }
 
 async function displayQuestion() {
   let question = await getQuestion().then();
-  console.log(question.incorrect_answers)
+  console.log(question.incorrect_answers);
 }
 
-displayQuestion()
-
-// At the time of making this comment, the OpenTDB (https://opentdb.com) API was down and I was struggling to find the structure that their objects follow to create mock data.
+displayQuestion();
+appendQuestion();
