44112603b19827f78d5102a964bca3de558fc0d7
cwitmer34 <connorwitmer34@gmail.com>
2022-08-01 19:55:10 -0400

    she's workin!!!

--- categories/animals/animals.js
+++ categories/animals/animals.js
@@ -5,7 +5,7 @@ const _answerTwo = document.getElementById("answer-2");
 const _answerThree = document.getElementById("answer-3");
 const _answerFour = document.getElementById("answer-4");
 const btns = document.querySelectorAll("button[id^=answer-]");
-var runCount = 0;
+var runCount = 1;
 var correct = 0;
 // Credits to my friend Jonah for teaching me how to cache data that I get from an API call.
 var triviaData = null;
@@ -30,85 +30,75 @@ const shuffleArray = (array) => {
   }
   return array;
 };
-async function triviaGame() {
-  const trivia = await getTrivia();
-  async function appendData() {
-    let totalAnswers = [
-      ...trivia.results[0].incorrect_answers,
-      trivia.results[0].correct_answer,
-    ];
-    // Apparently I need 2 different arrays to sort them because array variables are stored by reference? Learn something new everyday I guess.
-    let totalAnswers2 = [...totalAnswers];
-    let sorted = shuffleArray(totalAnswers2);
-    // Ensures the proper symbol shows instead of the HTML entities
-    const doc = new DOMParser().parseFromString(
-      trivia.results[0].question,
-      "text/html"
-    );
-    _questionHTML.textContent = doc.documentElement.textContent;
-    console.log(trivia.results[0].correct_answer, "- Correct Answer");
-    // Appends info to the DOM
-    _answerOne.textContent = sorted[0];
-    _answerTwo.textContent = sorted[1];
-    _answerThree.textContent = sorted[2];
-    _answerFour.textContent = sorted[3];
-  }
-  async function checkAnswer() {
-    btns.forEach((btn) => {
-      btn.addEventListener("click", (event) => {
-        console.log(event.target.textContent);
-        if (event.target.textContent === trivia.results[0].correct_answer) {
-          event.target.style.backgroundColor = "#52D452";
-          // Disables all buttons after one has been clicked.
-          btns.forEach((btn) => {
-            btn.disabled = true;
-          });
-          setTimeout(() => {
-            if (runCount === 10) {
-              return;
-            }
-            runCount++;
-            btns.forEach((btn) => {
-              btn.disabled = false;
-            });
-            btn.style.backgroundColor = "";
-            correct++;
-            document.getElementById(
-              "amount-correct"
-            ).textContent = `${correct}/10`;
-            triviaGame();
-          }, 2000);
-        } else {
-          event.target.style.backgroundColor = "#FF3D33";
-          // document.getElementById("correct-text").textContent =
-          //   trivia.results[0].correct_answer;
-          // document.getElementById("correct-answer").style.visibility =
-          //   "visible";
-          setTimeout(() => {
-            if (runCount === 10) {
-              return;
-            }
-            // document.getElementById("correct-answer").style.visibility =
-            //   "hidden";
-            btns.forEach((btn) => {
-              btn.disabled = false;
-            });
-            btn.style.backgroundColor = "";
-            runCount++;
-            triviaGame();
-          }, 3500);
-        }
-      });
-    });
-  }
-
-  checkAnswer();
-  appendData();
+async function appendData() {
+  triviaData = await getTrivia();
+  let totalAnswers = [
+    ...triviaData.results[0].incorrect_answers,
+    triviaData.results[0].correct_answer,
+  ];
+  // Apparently I need 2 different arrays to sort them because array variables are stored by reference? Learn something new everyday I guess.
+  let totalAnswers2 = [...totalAnswers];
+  let sorted = shuffleArray(totalAnswers2);
+  // Ensures the proper symbol shows instead of the HTML entities
+  const doc = new DOMParser().parseFromString(
+    triviaData.results[0].question,
+    "text/html"
+  );
+  _questionHTML.textContent = doc.documentElement.textContent;
+  console.log(triviaData.results[0].correct_answer, "- Correct Answer");
+  // Appends info to the DOM
+  _answerOne.textContent = sorted[0];
+  _answerTwo.textContent = sorted[1];
+  _answerThree.textContent = sorted[2];
+  _answerFour.textContent = sorted[3];
 }
 
-const iterate = () => {
-  triviaGame();
-  runCount++;
-};
+btns.forEach((btn) => {
+  btn.addEventListener("click", (event) => {
+    console.log(runCount);
+    if (event.target.textContent === triviaData.results[0].correct_answer) {
+      event.target.style.backgroundColor = "#52D452";
+      // Disables all buttons after one has been clicked.
+      btns.forEach((btn) => {
+        btn.disabled = true;
+      });
+      setTimeout(() => {
+        if (runCount === 10) {
+          return;
+        }
+        runCount++;
+        correct++;
+        btns.forEach((btn) => {
+          btn.disabled = false;
+        });
+        btn.style.backgroundColor = "";
+        document.getElementById("amount-correct").textContent = `${correct}/10`;
+        appendData();
+      }, 2000);
+    } else {
+      event.target.style.backgroundColor = "#FF3D33";
+      btns.forEach((btn) => {
+        btn.disabled = true;
+      });
+      // document.getElementById("correct-text").textContent =
+      //   trivia.results[0].correct_answer;
+      // document.getElementById("correct-answer").style.visibility =
+      //   "visible";
+      setTimeout(() => {
+        if (runCount === 10) {
+          return;
+        }
+        // document.getElementById("correct-answer").style.visibility =
+        //   "hidden";
+        btns.forEach((btn) => {
+          btn.disabled = false;
+          btn.style.backgroundColor = "";
+        });
+        runCount++;
+        appendData();
+      }, 3500);
+    }
+  });
+});
 
-iterate();
+appendData();
